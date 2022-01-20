456ef5ccf56b8b2df3b946c3e3f4a4e5b72b6801
X0bot <a.xlopot@gmail.com>
2022-01-20 14:01:56 +0200

    form with inputs

--- form_with_inputs/form.php
+++ form_with_inputs/form.php
@@ -14,15 +14,15 @@
         <input type="checkbox">
         <input type="color" name="color">
         <input type="tel" name="tel" class="form-control" placeholder="enter phone nymber">
-        <input type="time" class="form-control">
+        <input type="time" name="time" class="form-control">
         <input type="image" src="Hello_kitty_png.png" width="50" height="50">
         <input type="number">
         <input type="url" name="url" placeholder="paste url">
         <input type="radio">
-        <input type="datetime-local">
+        <input type="datetime-local" name="datetime">
         <input type="reset">
         <input type="file">
-        <input type="range" list="rangelist" min="0" max="100" step="1" value="78" class="mt-3">
+        <input type="range" name="ran" list="rangelist" min="0" max="100" step="1" value="78" class="mt-3">
         <datalist id="rangelist">
             <option value="0"></option>
             <option value="10"></option>
@@ -36,9 +36,9 @@
             <option value="90"></option>
             <option value="100"></option>
         </datalist>
-        <input type="month">
-        <input type="search">
-        <input type="week">
+        <input type="month" name="month">
+        <input type="search" name="search">
+        <input type="week" name="week">
         <input type="submit" value="validate">
     </form>
 </div>
--- form_with_inputs/validate.php
+++ form_with_inputs/validate.php
@@ -7,5 +7,10 @@ $pass = $_POST['password'];
 $email = $_POST['email'];
 $color = $_POST['color'];
 $tel = $_POST['tel'];
+$range = $_POST['ran'];
+$url = $_POST['url'];
+$datetime = $_POST['datetime'];
+$time = $_POST['time'];
+$week = $_POST['week'];
 
-echo $text."<br>".$date."<br>".$pass."<br>".$email."<br>".$color."<br>".$tel;
+echo $text."<br>".$date."<br>".$pass."<br>".$email."<br>".$color."<br>".$tel."<br>".$url."<br>".$datetime."<br>". $time."<br>".$week."<br>";
