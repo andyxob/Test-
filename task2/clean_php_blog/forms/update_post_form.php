23593a56d13375bed42fae9d8dd8eda4831c3eb9
X0bot <a.xlopot@gmail.com>
2022-01-24 15:25:52 +0200

    crud for posts

--- task2/clean_php_blog/forms/update_post_form.php
+++ task2/clean_php_blog/forms/update_post_form.php
@@ -1,18 +1,16 @@
-
 <html>
 <head>
     <link rel="stylesheet" type="text/css"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-<?php
-    require_once('../scripts/update_post.php');
-?>
+<?php include_once ("../scripts/update_post.php"); ?>
 <div class="container">
 <form method="post" action="../scripts/update_post.php" class="mt-5">
+    <input type="hidden" name= "id" value="<?php echo $id ?>">
     <input type="text" class="form-control"  placeholder="Title" name="title" value="<?php echo $title ; ?>"><br>
     <input type="text" class="form-control" placeholder="Description" name="description" value="<?php echo $description ; ?> "><br>
-    <input type="submit" class="btn btn-success" value="Edit">
+    <input type="submit" class="btn btn-success" name  = "update" value="Edit">
 </div>
 </form>
 
--- task2/clean_php_blog/scripts/update_post.php
+++ task2/clean_php_blog/scripts/update_post.php
@@ -2,20 +2,28 @@
 include("../connection.php");
 
 $conn = connect();
-
+$id = 0;
 $title= "";
 $description = "";
 
 if (isset($_GET['edit'])){
     $id = $_GET['edit'];
-    $results = mysqli_query($conn, "Select * from `posts` where `id` = '$id'");
-    if(count($results)==1){
-        $row = $results->fetch_array();
-        $title = $row['title'];
-        $description = $row['description'];
-    }
-echo"edit";
+    $results = mysqli_query($conn,"Select * from  `posts` where `id` = '$id'");
+    $row = mysqli_fetch_array($results);
+    $title = $row['title'];
+    $description = $row['description'];
+
 }
 
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $result = mysqli_query($conn, "update posts set title='$title', description='$description' where  id = '$id'");
+
+    header('location: ../admin_page.php');
+}
 close($conn);
-header('location : ../admin_page.php');
+
+?>
